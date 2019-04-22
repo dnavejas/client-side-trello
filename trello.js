2411763b2d911b6d6e597342440f26863641ed0c
Danny Navejas <dannynavejas@gmail.com>
2019-04-22 17:31:27 -0500

    added button that will call function to create a new card

--- trello.js
+++ trello.js
@@ -1,11 +1,24 @@
 'use strict'
 
 let addlaneID = 1;
+let cardID = 1;
 
-function addLane(){
-
+function addSection(){
+    addlaneID++;
+    let id = "addlane" + addlaneID;
     let newDiv = document.createElement("div");
-    newDiv.setAttribute("id", "addlane" + addlaneID);
+    newDiv.setAttribute("id", id);
     newDiv.setAttribute("class", "newlane");
     document.querySelector(".container").appendChild(newDiv);
+
+    
+        var cardBtn = document.createElement("input");
+        cardBtn.setAttribute("type", "button");
+        cardBtn.setAttribute("value", "Add Card");
+        cardBtn.setAttribute("id", "btnAddCard" + addlaneID)
+        cardBtn.setAttribute("data-addlane-id", addlaneID);
+        let newlane = document.querySelector("#" + id);
+        newlane.appendChild(cardBtn);
+        cardBtn.addEventListener("click", addCard);
+  
 }
